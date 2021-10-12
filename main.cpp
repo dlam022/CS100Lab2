6c1d4f8fa0bac98f888381f0b62efa05567fcea5
dlam022 <dericklam2@gmail.com>
2021-10-11 23:17:49 -0700

    Add file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <string>
+#include "c-echo.h"
+
+int main(int argv, char** argc) {
+	std::cout << "Count of output " << count(echo(argv,argc)) << std::endl;
+}
+
 
 int main() {
 {
-HEAD
+
     std::string name;
     std::cout << "What is your name?" << std::endl;
     std::cin >> name;
     std::cout << "Hello " << name << "!" << std::endl;
-
     return 0;
 }
+
+
+
+
